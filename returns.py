9c45507adfab0331665b3ad7fefaf1cade3e0b2a
bcm082 <gz4256@wayne.edu>
2023-02-14 18:46:42 -0500

    .gitignore, streamlit config, fix table sort

--- returns.py
+++ returns.py
@@ -6,23 +6,25 @@ import matplotlib.pyplot as plt
 # Create a form to collect the user's input
 with st.form(key='my_form', clear_on_submit=True):
     sku = st.text_input(label='SKU')
+    
     quantity = st.number_input(label='Quantity', min_value=1, value=1)
+    
     # Create a dropdown menu to select the reason for the return
     reason = st.selectbox(label='Reason', options=['Damaged', 'Wrong item', 'Too Small', ' Too Large','Changed Mind'])
+    
     # Save and Append the user's input to a csv file and clear the user's input afeter saving
     if st.form_submit_button(label='Submit'):
         df = pd.DataFrame({'SKU': [sku], 'Quantity': [quantity], 'Reason': [reason]})
         df.to_csv('returns.csv', mode='a', header=False, index=False)
         st.success('Submitted successfully')
 
-# Read the csv file and display the data in a table
+# Read the csv file and display the data in a table sorted by quantity
 df = pd.read_csv('returns.csv')
 
-# Display how many returns were made for each reason by sku and quantity in a table
-st.table(df.groupby(['sku', 'reason'])['quantity'].sum().head(10))
+# Display the data in a table sorted by quantity and combine the sku and reason columns
+st.table(df.groupby(['sku', 'reason'])['quantity'].sum().reset_index().sort_values(by='quantity', ascending=False).head(10))
 
 # Plot a bar of how many returns were made for each reason by sku and quantity
 st.bar_chart(df.groupby(['sku', 'reason'])['quantity'].sum().unstack().head(10))
-
 
 
