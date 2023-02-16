f5cb8f416a80ec033d9555e72ef12289e249172c
Bruno Martins <gz4256@wayne.edu>
2023-02-16 17:09:10 -0500

    Added 2002 Orders Page

---
+++ Issue_Tracker_Form.py
@@ -0,0 +1,64 @@
+# app.py, run with 'streamlit run app.py'
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import pymongo
+from streamlit_autorefresh import st_autorefresh
+
+
+
+# Initialize the MongoDb connection
+# Use st.cache to cache the connection so it doesn't have to be reinitialized every time the app is run
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient("mongodb+srv://"+st.secrets['username']+":"+st.secrets['password']+"@"+st.secrets['cluster_id']+".xq4stxw.mongodb.net/?retryWrites=true&w=majority")
+
+client = init_connection()
+
+# Connect to the returnsdb database and returns_collection collection
+db = client['returnsdb']
+collection = db['returns_collection']
+
+# Create a form to collect the user's input
+st.title('TVSO Customer Service Form')
+with st.form(key='returns_form', clear_on_submit=True):
+    sku_input = st.text_input(label='SKU')
+
+    # Create a number input to collect the quantity of the return
+    quantity = st.number_input(label='Quantity', min_value=1, max_value=100)
+
+    # Create a selectbox to collect the reason for the return
+    reason_input = st.selectbox(label='Reason', options=[' ', 'Damaged', 'Defective', 'Wrong Item', 'Missing Parts', 'Too Large', 'Too Small', 'Never Received'])
+
+    # Stop the user from submitting the form if the SKU or reason is blank
+    if sku_input == '' or reason_input == ' ':
+        st.form_submit_button('Submit', on_click=None, args=None, kwargs=None)
+    else:
+        # add a button to submit the form
+        if st.form_submit_button('Submit'):
+            # create a new document to save to MongoDB
+            doc = {
+                'SKU': sku_input,
+                'Reason': reason_input,
+                'Quantity': quantity
+            }
+            # insert the document into the MongoDB collection
+            collection.insert_one(doc)
+
+            # display a success message
+            st.success('Successfully saved')
+
+            # Auto refresh when the form is submitted
+            st_autorefresh()
+
+# Create a dataframe from the MongoDB returns_collection collection
+df = pd.DataFrame(list(collection.find()))
+
+# Display the data in a table sorted by quantity and combine the sku and reason columns
+st.table(df.groupby(['SKU', 'Reason']).sum().reset_index().sort_values('Quantity', ascending=False).head(25))
+
+# plot the data as a bar chart
+st.bar_chart(df.groupby(['SKU', 'Reason'])['Quantity'].sum(numeric_only=True).unstack().head(25))
+
+with st.sidebar:
+        st.write("")
---
+++ pages/Returns-2022.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create a dropdown to select the month of the data to display
+month = st.selectbox(label='Month', options=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+
+# If January is selected, display the data for January
+if month == 'January':
+    df = pd.read_csv('pages/data/January-2022.csv')
+# If February is selected, display the data for February
+elif month == 'February':
+    df = pd.read_csv('pages/data/February-2022.csv')
+# If March is selected, display the data for March
+elif month == 'March':
+    df = pd.read_csv('pages/data/March-2022.csv')
+# If April is selected, display the data for April
+elif month == 'April':
+    df = pd.read_csv('pages/data/April-2022.csv')
+# If May is selected, display the data for May
+elif month == 'May':
+    df = pd.read_csv('pages/data/May-2022.csv')
+# If June is selected, display the data for June
+elif month == 'June':
+    df = pd.read_csv('pages/data/June-2022.csv')
+# If July is selected, display the data for July
+elif month == 'July':
+    df = pd.read_csv('pages/data/July-2022.csv')
+# If August is selected, display the data for August
+elif month == 'August':
+    df = pd.read_csv('pages/data/August-2022.csv')
+# If September is selected, display the data for September
+elif month == 'September':
+    df = pd.read_csv('pages/data/September-2022.csv')
+# If October is selected, display the data for October
+elif month == 'October':
+    df = pd.read_csv('pages/data/October-2022.csv')
+# If November is selected, display the data for November
+elif month == 'November':
+    df = pd.read_csv('pages/data/November-2022.csv')
+# If December is selected, display the data for December
+elif month == 'December':
+    df = pd.read_csv('pages/data/December-2022.csv')
+
+
+
+
+# Display the data in a table sorted by quantity and combine the sku and reason columns
+st.table(df.groupby(['SKU', 'Reason']).sum().reset_index().sort_values('Quantity', ascending=False).head(25))
+
+# Display the top 25 SKUs by quantity in a table
+st.table(df.groupby('SKU').sum().reset_index().sort_values('Quantity', ascending=False).head(25))
