2b4df2f9b40d41795d06d62423e3527dfbef45d7
bcm082 <gz4256@wayne.edu>
2023-02-19 22:50:52 -0500

    Added Notes field

--- Issue_Tracker_Form.py
+++ Issue_Tracker_Form.py
@@ -30,6 +30,9 @@ with st.form(key='returns_form', clear_on_submit=True):
     # Create a selectbox to collect the reason for the return
     reason_input = st.selectbox(label='Reason', options=[' ', 'Damaged', 'Defective', 'Wrong Item', 'Missing Parts', 'Too Large', 'Too Small', 'Never Received'])
 
+    # Create a text area to collect any notes from the user
+    note_input = st.text_area(label='Notes')
+
     # Stop the user from submitting the form if the SKU or reason is blank
     if sku_input == '' or reason_input == ' ':
         st.form_submit_button('Submit', on_click=None, args=None, kwargs=None)
@@ -40,7 +43,8 @@ with st.form(key='returns_form', clear_on_submit=True):
             doc = {
                 'SKU': sku_input,
                 'Reason': reason_input,
-                'Quantity': quantity
+                'Quantity': quantity,
+                'Notes': note_input
             }
             # insert the document into the MongoDB collection
             collection.insert_one(doc)
@@ -54,8 +58,8 @@ with st.form(key='returns_form', clear_on_submit=True):
 # Create a dataframe from the MongoDB returns_collection collection
 df = pd.DataFrame(list(collection.find()))
 
-# Display the data in a table sorted by quantity and combine the sku and reason columns
-st.table(df.groupby(['SKU', 'Reason']).sum().reset_index().sort_values('Quantity', ascending=False).head(25))
+# Display the data in a table sorted by quantity and combine the sku and reason columns, ignoring the notes column
+st.table(df.groupby(['SKU', 'Reason', 'Notes']).sum().reset_index().sort_values('Quantity', ascending=False).head(25))
 
 # plot the data as a bar chart
 st.bar_chart(df.groupby(['SKU', 'Reason'])['Quantity'].sum(numeric_only=True).unstack().head(25))
