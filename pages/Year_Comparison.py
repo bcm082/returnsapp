c3956023f38176bfc589083003d96d0533d6c19c
Bruno Martins <gz4256@wayne.edu>
2023-05-05 10:28:29 -0400

    refactor

--- pages/Year_Comparison.py
+++ pages/Year_Comparison.py
@@ -5,59 +5,21 @@ import matplotlib.pyplot as plt
 # Create a title for the page
 st.title('Returns by Year Comparison')
 
-
 # Create a dropdown to select the month of the data to display
 month = st.selectbox(label='Select Month', options=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
 
+# Function to read data for the selected month and year
+def read_data(year, month):
+    file_path = f'pages/data/{year}/{month}-{year}.csv'
+    return pd.read_csv(file_path)
+
 # Read in the data for the selected month
-if month == 'January':
-    df_2021 = pd.read_csv('pages/data/2021/January-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/January-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/January-2023.csv')
-elif month == 'February':
-    df_2021 = pd.read_csv('pages/data/2021/February-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/February-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/February-2023.csv')
-elif month == 'March':
-    df_2021 = pd.read_csv('pages/data/2021/March-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/March-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/March-2023.csv')
-elif month == 'April':
-    df_2021 = pd.read_csv('pages/data/2021/April-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/April-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/April-2023.csv')
-elif month == 'May':
-    df_2021 = pd.read_csv('pages/data/2021/May-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/May-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/May-2023.csv')
-elif month == 'June':
-    df_2021 = pd.read_csv('pages/data/2021/June-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/June-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/June-2023.csv')
-elif month == 'July':
-    df_2021 = pd.read_csv('pages/data/2021/July-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/July-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/July-2023.csv')
-elif month == 'August':
-    df_2021 = pd.read_csv('pages/data/2021/August-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/August-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/August-2023.csv')
-elif month == 'September':
-    df_2021 = pd.read_csv('pages/data/2021/September-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/September-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/September-2023.csv')
-elif month == 'October':
-    df_2021 = pd.read_csv('pages/data/2021/October-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/October-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/October-2023.csv')
-elif month == 'November':
-    df_2021 = pd.read_csv('pages/data/2021/November-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/November-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/November-2023.csv')
-elif month == 'December':
-    df_2021 = pd.read_csv('pages/data/2021/December-2021.csv')
-    df_2022 = pd.read_csv('pages/data/2022/December-2022.csv')
-    df_2023 = pd.read_csv('pages/data/2023/December-2023.csv')
+try:
+    df_2021 = read_data('2021', month)
+    df_2022 = read_data('2022', month)
+    df_2023 = read_data('2023', month)
+except FileNotFoundError:
+    st.header('No Data Available')
 
 st.markdown("---")
 
